792bfa8d2f778e9fd64af649beeaed15483f5dad
Andrea Squillante <63291707+AndreaSquillante@users.noreply.github.com>
2023-05-11 17:48:13 +0200

    models

--- code/mergino.py
+++
@@ -1,9 +0,0 @@
-# Importiamo le librerie necessarie
-from sklearn.ensemble import VotingClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-
