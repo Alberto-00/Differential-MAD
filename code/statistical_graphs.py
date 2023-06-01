2a0945020b037c55cdad8706dda6a93db87e4b73
Alberto Montefusco <albmont99@gmail.com>
2023-06-01 13:22:49 +0200

    Update statistical_graphs.py

--- code/statistical_graphs.py
+++ code/statistical_graphs.py
@@ -3,93 +3,92 @@ import matplotlib.pyplot as plt
 
 #######################################################################################
 # Accuracy
-models_accuracy = ['Amsl', 'Amsl', 'Amsl', 'Amsl',
-                   'Facemorpher', 'Facemorpher', 'Facemorpher', 'Facemorpher',
-                   'OpenCV', 'OpenCV', 'OpenCV', 'OpenCV',
-                   'StyleGAN', 'StyleGAN', 'StyleGAN', 'StyleGAN',
-                   'Webmorph', 'Webmorph', 'Webmorph', 'Webmorph']
+models = ['Amsl', 'Amsl', 'Amsl', 'Amsl',
+          'Facemorpher', 'Facemorpher', 'Facemorpher', 'Facemorpher',
+          'OpenCV', 'OpenCV', 'OpenCV', 'OpenCV',
+          'StyleGAN', 'StyleGAN', 'StyleGAN', 'StyleGAN',
+          'Webmorph', 'Webmorph', 'Webmorph', 'Webmorph']
 
-sub_categories_accuracy = ['Tesisti', 'Nostri (not merged)', 'Merged', 'Smile (included)',
-                           'Tesisti', 'Nostri (not merged)', 'Merged', 'Smile (included)',
-                           'Tesisti', 'Nostri (not merged)', 'Merged', 'Smile (included)',
-                           'Tesisti', 'Nostri (not merged)', 'Merged', 'Smile (included)',
-                           'Tesisti', 'Nostri (not merged)', 'Merged', 'Smile (included)']
+sub_categories = ['Tesisti', 'Nostri (not merged)', 'Nostri (merged)', 'Smile (included)',
+                  'Tesisti', 'Nostri (not merged)', 'Nostri (merged)', 'Smile (included)',
+                  'Tesisti', 'Nostri (not merged)', 'Nostri (merged)', 'Smile (included)',
+                  'Tesisti', 'Nostri (not merged)', 'Nostri (merged)', 'Smile (included)',
+                  'Tesisti', 'Nostri (not merged)', 'Nostri (merged)', 'Smile (included)']
+
+models_accuracy_smile = ['Amsl', 'Facemorpher', 'OpenCV', 'StyleGAN', 'Webmorph']
+
+sub_categories_accuracy_smile = ['Smile (included)', 'Smile (included)', 'Smile (included)',
+                                 'Smile (included)', 'Smile (included)']
 
 # Tesisti, Nostri, Merged, Smile
-accuracy = [44, 98.32, 99.47, 97.89,
-            57, 83.52, 98.72, 98.79,
-            55, 87.94, 92.97, 93.20,
-            54, 96.35, 96.98, 96.75,
-            51, 92.29, 95.54, 96.37]
+accuracy_smile = [97.89, 98.79, 94.11, 96.75, 95.85]
 
-# ho preso solo l'eer del classificatore migliore
-# in base alla tabella riportata
-eer_pca = [0, 0.98, 0.29, 1.45,
-           0, 9.62, 2.04, 3.92,
-           0, 7.04, 3.81, 4.02,
-           0, 2.94, 4.33, 4.53,
-           0, 4.50, 2.42, 2.14]
+accuracy = [95.52, 98.32, 99.29, 97.89,
+            98.18, 83.52, 98.72, 98.79,
+            98.79, 88.29, 92.52, 94.11,
+            98.71, 96.21, 97.05, 96.75,
+            94.63, 91.16, 95.54, 95.85]
+
 
 #######################################################################################
+# Equal Erro Rate (EER) compared with paper
+models_eer_paper = ['Amsl', 'Amsl',
+                    'Facemorpher', 'Facemorpher',
+                    'OpenCV', 'OpenCV',
+                    'StyleGAN', 'StyleGAN',
+                    'Webmorph', 'Webmorph']
 
-# Error Equal rate (EER)
-models_apcr = ['Amsl', 'Amsl', 'Amsl',
-               'Facemorpher', 'Facemorpher', 'Facemorpher',
-               'OpenCV', 'OpenCV', 'OpenCV',
-               'StyleGAN', 'StyleGAN', 'StyleGAN',
-               'Webmorph', 'Webmorph', 'Webmorph']
+sub_categories_eer_paper = ['Nostri', 'Paper',
+                            'Nostri', 'Paper',
+                            'Nostri', 'Paper',
+                            'Nostri', 'Paper',
+                            'Nostri', 'Paper']
 
-sub_categories_apcr = ['Tesisti', 'Nostri', 'Paper',
-                       'Tesisti', 'Nostri', 'Paper',
-                       'Tesisti', 'Nostri', 'Paper',
-                       'Tesisti', 'Nostri', 'Paper',
-                       'Tesisti', 'Nostri', 'Paper']
+eer_paper = [12.74, 15.18,
+             0.49, 3.87,
+             0.57, 4.39,
+             9.82, 8.99,
+             7.53, 12.35]
 
-# Tesisti, Nostri, Paper
-eer = [52.32, 12.74, 15.18,
-       5.23, 0.49, 3.87,
-       5.15, 0.57, 4.39,
-       0.0, 9.82, 8.99,
-       5.24, 7.53, 12.35]
-
+#######################################################################################
 # BPCR (0.10%) @ APCR =
-bpcr01 = [66.43, 100.00, 76.47,
-          5.56, 100.00, 63.24,
-          5.48, 100.00, 64.22,
-          0.0, 100.00, 79.90,
-          5.56, 100.00, 90.69]
+bpcr01 = [58.82, 100, 100, 100,
+          63.73, 100, 100, 100,
+          71.08, 100, 100, 100,
+          79.90, 100, 100, 100,
+          74.51, 100, 100, 100]
 
 # BPCR (1.00%) @ APCR =
-bpcr1 = [63.95, 68.78, 49.51,
-         5.40, 0.49, 23.53,
-         5.24, 0.57, 26.47,
-         0.0, 41.73, 42.16,
-         5.40, 43.90, 80.39]
+bpcr1 = [33.33, 49.02, 0, 27.94,
+         26.47, 0, 52.94, 100,
+         22.06, 0, 0, 0,
+         42.16, 37.74, 16.70, 15.69,
+         53.92, 1.47, 0.98, 1.96]
 
 # BPCR (10.00%) @ APCR =
-bpcr10 = [59.81, 15.86, 21.08,
-          5.23, 0.16, 0.49,
-          5.15, 0.57, 1.96,
-          0.0, 9.98, 8.82,
-          5.15, 6.96, 15.20]
+bpcr10 = [9.80, 0, 0, 0,
+          0.49, 0, 52.94, 100,
+          0.49, 0, 0, 0,
+          8.82, 0, 0, 0,
+          9.80, 0, 0, 0]
 
 # BPCR (20.00%) @ APCR =
-bpcr20 = [58.20, 7.82, 11.76,
-          5.07, 0.16, 0.49,
-          4.99, 0.57, 1.47,
-          0.0, 5.16, 4.41,
-          5.15, 4.59, 7.84]
+bpcr20 = [4.41, 0, 0, 0,
+          0, 0, 52.94, 100,
+          0.49, 0, 0, 0,
+          4.41, 0, 0, 0,
+          2.94, 0, 0, 0]
 
 #######################################################################################
 
 # Creazione del grafico a colonne con sottocategorie
-sns.barplot(x=models_accuracy, y=accuracy, hue=sub_categories_accuracy,
-            color='blue', saturation=0.7, alpha=0.8)
+sns.barplot(x=models, y=bpcr20, hue=sub_categories,
+            palette='pastel', saturation=0.7, alpha=0.8)
 
 # Aggiunta di etichette
 plt.xlabel('Models')
-plt.ylabel('Accuracy')
-plt.title('Accuracy')
+plt.ylabel('%')
+plt.title('BPCR (20.00%) @ APCR =')
 
 # Modifica della grandezza delle colonne
 plt.xticks(fontsize=10)
