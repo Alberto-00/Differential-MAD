003a700c565887c3f61c3b85482b71bed4a22003
AlessandroUnisa <96615359+AlessandroUnisa@users.noreply.github.com>
2023-06-12 10:29:33 +0200

    valid graphs and err with paper

--- code/statistical_graphs.py
+++ code/statistical_graphs.py
@@ -2,28 +2,30 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 #######################################################################################
-models = ['Amsl', 'Amsl', 'Amsl',
-          'Facemorpher', 'Facemorpher', 'Facemorpher',
-          'OpenCV', 'OpenCV', 'OpenCV',
-          'StyleGAN', 'StyleGAN', 'StyleGAN',
-          'Webmorph', 'Webmorph', 'Webmorph']
+models = ['Amsl', 'Amsl', 'Amsl', 'Amsl',
+          'Facemorpher',  'Facemorpher', 'Facemorpher', 'Facemorpher',
+          'OpenCV', 'OpenCV', 'OpenCV', 'OpenCV',
+          'StyleGAN', 'StyleGAN', 'StyleGAN', 'StyleGAN',
+          'Webmorph', 'Webmorph', 'Webmorph', 'Webmorph']
 
-sub_categories_no_smile = ['Approccio Geometrico (not Smile)', 'AoM not Smile',
+
+
+sub_categories_no_smile = ['Paper', 'Approccio Geometrico (not Smile)', 'AoM not Smile',
                            'AoM & Approccio Geometrico (not Smile)',
-                           'Approccio Geometrico (not Smile)', 'AoM not Smile',
+                           'Paper', 'Approccio Geometrico (not Smile)', 'AoM not Smile',
                            'AoM & Approccio Geometrico (not Smile)',
-                           'Approccio Geometrico (not Smile)', 'AoM not Smile',
+                           'Paper','Approccio Geometrico (not Smile)', 'AoM not Smile',
                            'AoM & Approccio Geometrico (not Smile)',
-                           'Approccio Geometrico (not Smile)', 'AoM not Smile',
+                           'Paper', 'Approccio Geometrico (not Smile)', 'AoM not Smile',
                            'AoM & Approccio Geometrico (not Smile)',
-                           'Approccio Geometrico (not Smile)', 'AoM not Smile',
+                           'Paper', 'Approccio Geometrico (not Smile)', 'AoM not Smile',
                            'AoM & Approccio Geometrico (not Smile)']
 
-sub_categories_smile = ['Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)',
-                        'Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)',
-                        'Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)',
-                        'Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)',
-                        'Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)']
+sub_categories_smile = ['Paper', 'Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)',
+                        'Paper', 'Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)',
+                        'Paper', 'Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)',
+                        'Paper', 'Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)',
+                        'Paper', 'Approccio Geometrico (Smile)', 'AoM Smile', 'AoM & Approccio Geometrico (Smile)']
 
 # Accuracy - NO SMILE
 accuracy_no_smile = [95.52, 17.52, 95.76,
@@ -60,18 +62,18 @@ eer_paper = [12.74, 15.18,
              7.53, 12.35]
 
 # EER - NO SMILE
-eer_no_smile = [9.80, 43.17, 3.66,
-                2.20, 2.41, 0.39,
-                2.42, 3.38, 2.70,
-                8.99, 16.16, 3.43,
-                9.86, 41.15, 3.80]
+eer_no_smile = [15.18, 9.80, 43.17, 3.66,
+                3.87, 2.20, 2.41, 0.39,
+                4.39, 2.42, 3.38, 2.70,
+                8.99, 8.99, 16.16, 3.43,
+                12.35, 9.86, 41.15, 3.80]
 
 # EER - SMILE
-eer_smile = [48.91, 42.46, 4.77,
-             47.70, 0.98, 3.92,
-             49.14, 1.22, 3.92,
-             45.66, 14.57, 4.06,
-             47.82, 40.29, 3.97]
+eer_smile = [15.18, 48.91, 42.46, 4.77,
+             3.87, 47.70, 0.98, 3.92,
+             4.39, 49.14, 1.22, 3.92,
+             8.99, 45.66, 14.57, 4.06,
+             12.35, 47.82, 40.29, 3.97]
 
 #######################################################################################
 # *********  NO SMILE *********************
@@ -136,8 +138,34 @@ bpcr20_smile = [48.52, 1.47, 5.88,
 
 #######################################################################################
 
+# *********  Validation *********************
+models_v = ['Amsl', 'Amsl', 'Amsl', 'Amsl', 'Amsl',
+           'Facemorpher', 'Facemorpher',  'Facemorpher', 'Facemorpher', 'Facemorpher',
+          'OpenCV', 'OpenCV', 'OpenCV', 'OpenCV', 'OpenCV',
+          'StyleGAN', 'StyleGAN', 'StyleGAN', 'StyleGAN', 'StyleGAN',
+          'Webmorph', 'Webmorph', 'Webmorph', 'Webmorph', 'Webmorph']
+
+lines_name_v = ['Amsl', 'Facemorpher', 'OpenCV', 'StyleGAN','Webmorph',
+                'Amsl', 'Facemorpher', 'OpenCV', 'StyleGAN','Webmorph',
+                'Amsl', 'Facemorpher', 'OpenCV', 'StyleGAN','Webmorph',
+                'Amsl', 'Facemorpher', 'OpenCV', 'StyleGAN','Webmorph',
+                'Amsl', 'Facemorpher', 'OpenCV', 'StyleGAN','Webmorph']
+
+
+eer_validation_smile = [42.46, 50, 50, 49.51, 49.91,
+                        0.98, 9.62, 10.64, 3.64, 6.26,
+                        1.22, 24.82, 25.22, 10.38, 16.40,
+                        14.57, 35.68, 35.02, 22.05, 28.89,
+                        40.29, 49.96, 49.96, 50, 49.84]
+
+eer_validation_no_smile = [43.17, 50, 50, 49.53, 49.91,
+                           2.41, 10.02, 11.37, 4.13, 7.45,
+                           3.38, 25.10, 25.79, 11.11, 17.09,
+                           16.16, 35.84, 35.35, 25.53, 29.54,
+                           41.15, 49.96, 49.96, 50.04, 49.84]
+
 # Creazione del grafico a colonne con sottocategorie
-ax = sns.barplot(x=models, y=bpcr20_smile, hue=sub_categories_smile,
+ax = sns.barplot(x=models, y=eer_no_smile, hue=sub_categories_no_smile,
                  palette='pastel', saturation=0.7, alpha=0.8)
 
 # Aggiungi il valore sulle barre
@@ -151,8 +179,8 @@ fig.set_size_inches(15, 11)
 
 # Aggiunta di etichette
 plt.xlabel('Tests', fontsize=20)
-plt.ylabel('BPCER (20.00%) @ APCER (%)', fontsize=20)
-plt.title("BPCER (20.00%) @ APCER (con 'smile')", fontsize=20)
+plt.ylabel('EER (%)', fontsize=20)
+plt.title("Equal Error Rate(no 'smile')", fontsize=20)
 
 # Modifica della grandezza delle colonne
 plt.xticks(fontsize=16)
@@ -160,6 +188,27 @@ plt.yticks(fontsize=16)
 
 # Posiziona la leggenda fuori dal grafico
 plt.legend(loc="upper right", bbox_to_anchor=(1.0, 1.16), fontsize=12)
+
+# Visualizzazione del grafico
+plt.show()
+
+
+sns.lineplot(x=models_v, y=eer_validation_no_smile, hue=lines_name_v)
+# Grandezza grafico
+fig = plt.gcf()
+fig.set_size_inches(15, 11)
+
+# Aggiunta di etichette
+plt.xlabel('Validation', fontsize=20)
+plt.ylabel('EER (%)', fontsize=20)
+plt.title("EER a variare del Validation (no_smile)", fontsize=20)
+
+# Modifica della grandezza delle colonne
+plt.xticks(fontsize=16)
+plt.yticks(fontsize=16)
+
+# Posiziona la leggenda fuori dal grafico
+plt.legend(loc="upper right", bbox_to_anchor=(1.0, 1.19), fontsize=12)
 
 # Visualizzazione del grafico
 plt.show()
